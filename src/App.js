94bf826b968a3179f18cd7c43dd69cba82e01d01
Gil Pratte <gil.pratte@mutualmobile.com>
2019-08-10 17:10:49 -0500

    trying to get the add todo to work

--- src/App.js
+++ src/App.js
@@ -3,18 +3,44 @@ import './App.css';
 import Reminder from './Reminder';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: [{"key": new Date().getTime(), "description":"uno", "done":false}],
+    };
+  }
+
+  addTodo() {
+    console.log("adding todo")
+    const todos = [...this.state.todos];
+    todos.unshift({"key": new Date().getTime(), "description":null, "done":false})
+    console.log("setting new state " + JSON.stringify(todos))
+    this.setState({todos:todos});
+  }
+
+  debugMe() {
+    console.log(this.state.todos);
+  }
+
+  renderTodo(todo) {
+    console.log("render to do " + JSON.stringify(todo))
+    return (
+      <Reminder value={todo} />
+    );
+  }
+
   render() {
     return (
       <div>
         <div className={"header"}>
           <h1>Reminders</h1>
-          <button className="header-item">New Reminder</button>
+          <button className="header-item" onClick={() => this.addTodo()}>New Reminder</button>
           <button className="header-item">Hide Done</button>
         </div>
         <div>
           <ul>
-            <Reminder/>
-            <Reminder/>
+          <Reminder value={{"key": 34, "description":"hmm", "done":false}} />
+          {this.renderTodo(this.state.todos[0])}
           </ul>
         </div>
       </div>
--- src/Reminder.js
+++ src/Reminder.js
@@ -2,11 +2,20 @@ import React from 'react';
 import './Reminder.css';
 
 class Reminder extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log("in reminder constructor " + JSON.stringify(props))
+    this.state = {
+      key: props.value.key,
+      description: props.value.description,
+      done: props.value.done
+    };
+  }
   render() {
     return (
-      <li>
-        <input className="done" type="checkbox" checked></input>
-        <input type="text" value="buy milk"></input>
+      <li key={this.state.key}>
+        <input className="done" type="checkbox" defaultChecked></input>
+        <input type="text" value={this.state.description}></input>
         <button>x</button>
       </li>
     );
